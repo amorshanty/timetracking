22ab225b758a2764d9d097dafb2cea174d7b9806
Amor <amor.dibella@gmail.com>
2015-02-02 12:34:16 +0100

    primer commit semana 4

--- db/migrate/20150202105643_create_proyects.rb
+++ db/migrate/20150202105643_create_proyects.rb
@@ -1,9 +1,7 @@
 class CreateProyects < ActiveRecord::Migration
   def change
     create_table :proyects do |t|
-
     	t.string :name
-    	t.string :description
 
       t.timestamps null: false
     end
